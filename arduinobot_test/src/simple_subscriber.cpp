b1270c0434274e817f6267c44e7cb0f9588788ae
Gaurish Gangwar <gaurishgangwar@gmail.com>
2023-06-15 15:52:06 +0900

    Added subscriber in C++

---
+++ arduinobot_test/src/simple_subscriber.cpp
@@ -0,0 +1,15 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+
+void msgCallback(const std_msgs::String::ConstPtr msg)
+{
+    ROS_INFO("I heard %s", msg->data.c_str());
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "listener");
+    ros::NodeHandle nh;
+    ros::Subscriber sub = nh.subscribe<std_msgs::String>("chatter", 10, msgCallback);
+    ros::spin();
+}
